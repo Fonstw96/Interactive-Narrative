3c811b35142689c2a83a6b7b5d7dd6b4fe927dfb
Unknown <fons_met_het_lange_e-mailadres@abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk.com>
2018-04-11 23:49:35 +0200

    Demo af!
    1 level af? Knippen en plakken maar!

---
+++ Assets/Wereld/Code/Vervagen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Vervagen : MonoBehaviour
+{
+    public float aantalSeconden;
+    private Image ik;
+
+    void Start()
+    {
+        ik = GetComponent<Image>();
+    }
+
+    void Update ()
+    {
+        float doorzichtigheid = 1.0f - Time.time / aantalSeconden;
+
+        if (doorzichtigheid > 0)
+            ik.color = new Color(1, 1, 1, doorzichtigheid);
+        else
+            gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Wereld/Code/VolgendeScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class VolgendeScene : MonoBehaviour
+{
+    public int sceneNummer;
+
+	void Start ()
+    {
+        GetComponent<Button>().onClick.AddListener(delegate () { SceneManager.LoadScene(sceneNummer); });
+	}
+}
