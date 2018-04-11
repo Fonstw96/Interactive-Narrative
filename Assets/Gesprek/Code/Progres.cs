b09d08e6c5fd7fb88741bf1caef1e5997e062313
Unknown <fons_met_het_lange_e-mailadres@abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk.com>
2018-04-11 13:39:28 +0200

    Gesprekscene krijgt vorm

---
+++ Assets/Gesprek/Code/Progres.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Progres : MonoBehaviour
+{
+    public float blijheid = .5f;
+    public Image ontevredenBalk;
+
+	public void VeranderBlijheid(float nieuweBlijheid)
+    {
+        blijheid = nieuweBlijheid;
+
+        ontevredenBalk.fillAmount = blijheid;
+    }
+}
