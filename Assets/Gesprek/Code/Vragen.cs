2b55f6f7a54e57c1cf61522e596875907e0590cf
Unknown <fons_met_het_lange_e-mailadres@abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk.com>
2018-04-11 14:45:51 +0200

    Gesprekscene reageert
    Tenminste, is nog niet klikbaar

---
+++ Assets/Gesprek/Code/Vragen.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[System.Serializable]
+
+public class StringList
+{
+    public string[] antwoorden;
+}
+
+public class Vragen : MonoBehaviour
+{
+    public string[] teksten;
+    public string[] vragen;
+    public StringList[] opties;
+
+    public Text spreekVak;
+    public Text dialoogVak;
+    public Text[] dialoogKnoppen;
+
+    private int progres = 0;
+
+    private void Start()
+    {
+        VolgendeRonde();
+    }
+
+    public void VolgendeRonde()
+    {
+        VulElementen(progres);
+        progres++;
+    }
+
+    private void VulElementen(int p)
+    {
+        spreekVak.text = teksten[p];
+        dialoogVak.text = vragen[p];
+
+        int index = 0;
+        foreach (Text knop in dialoogKnoppen)
+        {
+            knop.text = opties[p].antwoorden[index];
+            index++;
+        }
+    }
+}
